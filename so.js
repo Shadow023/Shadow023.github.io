4bf4a5bae9ae848c832765cf6eef5622bbefcf4a
Shadow023 <78748588+Shadow023@users.noreply.github.com>
2021-02-08 21:19:51 +0500

    Add files via upload

---
+++ so.js
@@ -0,0 +1,8 @@
+function secret()
+{
+    x=document.getElementsByClassName("OPEN");
+    x[0].style.visibility="visible";
+    x[1].style.visibility="visible";
+    y=document.getElementById('secret');
+    y.style.display="none";
+}
